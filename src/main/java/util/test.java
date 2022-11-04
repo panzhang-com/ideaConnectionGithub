91fb9ebf83dbf36c3d0e15fca62eea44e8d0149f
panzhang-com <2744687886@qq.com>
2022-11-04 17:09:34 +0800

    change local

--- src/main/java/util/test.java
+++ src/main/java/util/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("修改");
         System.out.println("在线更新");
+        System.out.println("本地更新");
     }
 }
